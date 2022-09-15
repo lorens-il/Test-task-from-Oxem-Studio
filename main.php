44744bcf4c0c22f6f9f58af02f6fff7898b27273
lawrence-il <lawrence.il.pr@gmail.com>
2022-09-15 16:16:49 +0500

    fix

--- main.php
+++ main.php
@@ -11,15 +11,15 @@
     $cow = new Cow();
 
     //Система должна добавить животных в хлев (10 коров и 20 кур).
-    $farm->addAnimals(20, Cow::getAnimalType());
-    $farm->addAnimals(10, Chicken::getAnimalType());
+    $farm->addAnimals(10, Cow::getAnimalType());
+    $farm->addAnimals(20, Chicken::getAnimalType());
 
     echo "\nВывести на экран информацию о количестве каждого типа животных на ферме.\n";
     echo $farm->printAnimalsQuantityEachType();
 
     echo "\nВывести на экран общее кол-во собранных за неделю шт. яиц и литров молока.\n";
-    $farm->collectedGoods($chicken, 0, 1);
     $farm->collectedGoods($cow, 8, 12);
+    $farm->collectedGoods($chicken, 0, 1);
 
     // Добавить на ферму ещё 5 кур и 1 корову (съездили на рынок, купили животных).
     $farm->addAnimals(1, Cow::getAnimalType());
@@ -29,7 +29,7 @@
     echo $farm->printAnimalsQuantityEachType();
 
     echo "\nСнова 7 раз (неделю) производим сбор продукции и выводим результат на экран.\n";
-    $farm->collectedGoods($chicken, 0, 1);
     $farm->collectedGoods($cow, 8, 12);
+    $farm->collectedGoods($chicken, 0, 1);
     
 ?>
